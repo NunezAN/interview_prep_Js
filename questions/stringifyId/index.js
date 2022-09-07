b6fb6a85edf0f5209bd3b47ca301f58ff097834a
NunezAN <alex101nunez@gmail.com>
2022-09-06 22:05:47 -0500

    solved stringifyId using set to remove duplicates

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,14 +17,19 @@
  */
 
 const stringifyId = (arr) => {
-    let uniqueArray = [];
-    for(let i=0;i<arr.length;i++){
-        if(!uniqueArray.includes(arr[i].id))
-        {
-            uniqueArray.push(arr[i].id);
-        }
-    }
+    // let uniqueArray = [];
+    // for(let i=0;i<arr.length;i++){
+    //     if(!uniqueArray.includes(arr[i].id))
+    //     {
+    //         uniqueArray.push(arr[i].id);
+    //     }
+    // }
+    // return uniqueArray.join(", ");
+
+    const idArray = arr.map(elem => elem.id);
+    const uniqueArray = [...new Set(idArray)];
     return uniqueArray.join(", ");
+
 };
 
 module.exports = stringifyId;
