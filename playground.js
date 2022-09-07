e6600b8a68f1613319d960be3806be485435589f
NunezAN <alex101nunez@gmail.com>
2022-09-06 21:20:20 -0500

    solved stringify id using for loop

--- playground.js
+++ playground.js
@@ -1,9 +1,12 @@
-const reverseInteger = (int) => {
-    reverseIntString = int.toString().split("").reverse().join("");
-    if(int < 0){
-        reverseIntString = "-" + reverseIntString;
+const stringifyId = (arr) => {
+    let uniqueArray = [];
+    for(let i=0;i<arr.length;i++){
+        if(!uniqueArray.includes(arr[i].id))
+        {
+            uniqueArray.push(arr[i].id);
+        }
     }
-    return parseInt(reverseIntString);
+    return uniqueArray.join();
 };
 
-console.log(reverseInteger(-120));
+console.log(stringifyId([{ id: 1 }, { id: 2 }, { id: 2 }, { id: 3 }]));
--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,7 +17,14 @@
  */
 
 const stringifyId = (arr) => {
-
+    let uniqueArray = [];
+    for(let i=0;i<arr.length;i++){
+        if(!uniqueArray.includes(arr[i].id))
+        {
+            uniqueArray.push(arr[i].id);
+        }
+    }
+    return uniqueArray.join(", ");
 };
 
 module.exports = stringifyId;
