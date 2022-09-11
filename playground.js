484c0e896db5d62747d75f4b992074aba20f0c7e
NunezAN <alex101nunez@gmail.com>
2022-09-11 10:22:01 -0500

    solved anagram using charMap and nested for loop

--- playground.js
+++ playground.js
@@ -1,17 +1,28 @@
-const maxCharacter = (str) => {
-    const charMap = new Map();
-    let maxCharacter = "";
-    let maxCount = 0;
-    for(const char of str){
-        const count = charMap.get(char);
-        charMap.set(char, count+1 || 1);
-        if(maxCount < charMap.get(char)){
-            maxCharacter = char;
-            maxCount = charMap.get(char);
+const anagrams = (strA, strB) => {
+  let charMapA = {};
+  let charMapB = {};
+  let anagram = false;
+  for (let char of strA.toUpperCase()) {
+    charMapA[char] = charMapA[char] ? charMapA[char] + 1 : 1;
+  }
+  for (let char of strB.toUpperCase()) {
+    charMapB[char] = charMapB[char] ? charMapB[char] + 1 : 1;
+  }
+  if (Object.keys(charMapA).length === Object.keys(charMapB).length) {
+    for (let elemA in charMapA) {
+      for (let elemB in charMapB) {
+        if (elemA === elemB) {
+          if (charMapA[elemA] !== charMapB[elemB]) {
+            anagram = false;
+            return anagram;
+          } else {
+            anagram = true;
+          }
         }
+      }
     }
-    return maxCharacter;
-
+  }
+  return anagram;
 };
 
-console.log(maxCharacter("foo 111123"));
+console.log(anagrams("save", "vase"));
--- questions/anagrams/index.js
+++ questions/anagrams/index.js
@@ -10,7 +10,34 @@
  */
 
 const anagrams = (strA, strB) => {
-  
-}
+  let charMapA = {};
+  let charMapB = {};
+  let anagram = false;
+  for (let char of strA.toUpperCase()) {
+    if (char !== " ") {
+      charMapA[char] = charMapA[char] ? charMapA[char] + 1 : 1;
+    }
+  }
+  for (let char of strB.toUpperCase()) {
+    if (char !== " ") {
+      charMapB[char] = charMapB[char] ? charMapB[char] + 1 : 1;
+    }
+  }
+  if (Object.keys(charMapA).length === Object.keys(charMapB).length) {
+    for (let elemA in charMapA) {
+      for (let elemB in charMapB) {
+        if (elemA === elemB) {
+          if (charMapA[elemA] !== charMapB[elemB]) {
+            anagram = false;
+            return anagram;
+          } else {
+            anagram = true;
+          }
+        }
+      }
+    }
+  }
+  return anagram;
+};
 
 module.exports = anagrams;
