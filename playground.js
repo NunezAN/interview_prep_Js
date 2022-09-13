49a702fb3bfaf30741e149ce15b82f8e56f3d1fb
NunezAN <alex101nunez@gmail.com>
2022-09-13 13:14:06 -0500

    completed steps using nested for loop

--- playground.js
+++ playground.js
@@ -1,14 +1,16 @@
-const steps = (n) => {
-  for (let i = 0; i < n; i++) {
-    let str = "";
-    for (let y = 0; y < n; y++) {
-      if (y > i) {
-        str = str + " ";
+const pyramid = (n) => {
+  columnWidth = 2 * n - 1;
+  for (let row = 0; row < n; row++) {
+    let pyramidColumn = "";
+    for (let column = 0; column <= columnWidth; column++) {
+      middle = Math.floor(columnWidth / 2);
+      if (column>= middle -row && column <= middle + row) {
+        pyramidColumn = pyramidColumn + "#";
       } else {
-        str = str + "#";
+        pyramidColumn = pyramidColumn + " ";
       }
     }
-    console.log(str);
+    console.log(pyramidColumn);
   }
 };
-console.log(steps(3));
+console.log(pyramid(3));
--- questions/pyramid/index.js
+++ questions/pyramid/index.js
@@ -1,5 +1,5 @@
 /** Print out steps for a pyramid based on a given positive integer N.
- * For each line, print out the character '#' to demonstrate a step 
+ * For each line, print out the character '#' to demonstrate a step
  * and make sure to use spaces to fill in the line.
  *
  * @example
@@ -18,7 +18,19 @@
  */
 
 const pyramid = (n) => {
-  
+    columnWidth = 2 * n - 1;
+    for (let row = 0; row < n; row++) {
+      let pyramidColumn = "";
+      for (let column = 0; column <= columnWidth; column++) {
+        middle = Math.floor(columnWidth / 2);
+        if (column>= middle -row && column <= middle + row) {
+          pyramidColumn = pyramidColumn + "#";
+        } else {
+          pyramidColumn = pyramidColumn + " ";
+        }
+      }
+      console.log(pyramidColumn);
+    }
 };
 
 module.exports = pyramid;
