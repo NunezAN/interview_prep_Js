04b10d7c2e338c99598b01448241136b7a7b59b8
NunezAN <alex101nunez@gmail.com>
2022-09-16 13:01:22 -0500

    completed clock question

---
+++ clock/index.js
@@ -0,0 +1,36 @@
+const millisecondHandle = document.querySelector(".handleMilliseconds");
+const secondHandle = document.querySelector(".handleSeconds");
+const minuteHandle = document.querySelector(".handleMinute");
+const hourHandle = document.querySelector(".handleHour");
+function main() {
+  requestAnimationFrame(updateClock);
+}
+
+function updateClock() {
+  const time = new Date(Date.now());
+  timeHour = time.getHours();
+  timeMinutes = time.getMinutes();
+  timeSeconds = time.getSeconds();
+  timeMilliseconds = time.getMilliseconds();
+
+  const minutesScale = 6 / 60;
+  const extraMinutesAngle = timeSeconds * minutesScale;
+  const minutesAngle = timeMinutes * 6 + extraMinutesAngle;
+
+  const HourScale = 360 / 12 / 60;
+  const extaHoursAngle = timeMinutes * HourScale;
+  const HoursAngle = timeHour * HourScale + extaHoursAngle;
+
+  millisecondHandle.style.transform = `translateX(-50%) rotate(${
+    timeMilliseconds * (360 / 1000)
+  }deg)`;
+  secondHandle.style.transform = `translateX(-50%) rotate(${
+    timeSeconds * (360 / 60)
+  }deg)`;
+  minuteHandle.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`;
+  hourHandle.style.transform = `translateX(-50%) rotate(${HoursAngle}deg)`;
+  //   console.log(timeHour);
+
+  requestAnimationFrame(updateClock);
+}
+main();
