ce05e96058fa835063c34cd7af1ec69fc8f3ba31
NunezAN <alex101nunez@gmail.com>
2022-09-18 13:57:23 -0500

    completed twoSum using map

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,19 +1,13 @@
 const twoSum = (nums, target) => {
   twoSumArray = [];
-
+  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    const remain = (target - nums[i]);
-    console.log(remain);
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] === remain) {
-        console.log("FOUND MATCH!!");
-        twoSumArray.push(i);
-        twoSumArray.push(j);
-        return twoSumArray;
-      }
+    const remain = target - nums[i];
+    if (map.get(nums[i])!==undefined){
+      return [map.get(nums[i]),i];
     }
+    map.set(remain,i);
   }
-  return twoSumArray;
 };
 
 console.log(twoSum([10, 20], 30));
