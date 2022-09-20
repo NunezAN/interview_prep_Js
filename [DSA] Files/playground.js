fb986bde4f412998ceb410712b4203b187bf39b3
NunezAN <alex101nunez@gmail.com>
2022-09-20 14:45:16 -0500

    completed three sum

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,21 +1,37 @@
-const stocks = (prices) => {
-  let l = 0;
-  let r = l + 1;
-  let max = 0;
-  while (r < prices.length) {
-    const profit = prices[r] - prices[l];
-    console.log(l,r)
-    if (profit > 0) {
-      if (max < profit) {
-        max = profit;
+function threeSum(nums) {
+  solutionArray = [];
+  nums = nums.sort(function (a, b) {
+    return a - b;
+  });
+  // console.log(nums);
+  i = 0;
+  l = i + 1;
+  r = nums.length - 1;
+  while (i < nums.length - 2) {
+    // (console.log(negative);
+    if (nums[i] !== nums[i - 1]) {
+      while (l < r) {
+        // console.log(i,l,r);
+        let sum = nums[l] + nums[r] + nums[i];
+        if (0 === sum) {
+          solutionArray.push([nums[i], nums[l], nums[r]]);
+          l++;
+          while(nums[l]===nums[l-1] && l<r)
+          {
+            l++;
+          }
+        } else if (sum < 0) {
+          l++;
+        } else if (sum > 0) {
+          r--;
+        }
       }
     }
-    else{
-        l=r;
-    }
-    r++;
+    i++;
+    l = i + 1;
+    r = nums.length - 1;
   }
-  return max;
-};
+  return solutionArray;
+}
 
-console.log(stocks([8, 3, 6, 1, 6, 4, 7]));
+console.log(threeSum([-2, -2, 0, 0, 1, 1, 2, 2]));
--- [DSA] Files/questions/threeSum/index.js
+++ [DSA] Files/questions/threeSum/index.js
@@ -12,7 +12,38 @@
  */
 
 function threeSum(nums) {
-  
+  solutionArray = [];
+  nums = nums.sort(function (a, b) {
+    return a - b;
+  });
+  // console.log(nums);
+  i = 0;
+  l = i + 1;
+  r = nums.length - 1;
+  while (i < nums.length - 2) {
+    // (console.log(negative);
+    if (nums[i] !== nums[i - 1]) {
+      while (l < r) {
+        // console.log(i,l,r);
+        let sum = nums[l] + nums[r] + nums[i];
+        if (0 === sum) {
+          solutionArray.push([nums[i], nums[l], nums[r]]);
+          l++;
+          while (nums[l] === nums[l - 1] && l < r) {
+            l++;
+          }
+        } else if (sum < 0) {
+          l++;
+        } else if (sum > 0) {
+          r--;
+        }
+      }
+    }
+    i++;
+    l = i + 1;
+    r = nums.length - 1;
+  }
+  return solutionArray;
 }
 
 //DO NOT EDIT BELOW THIS LINE
