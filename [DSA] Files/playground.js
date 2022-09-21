66016997d44e890f2fa81c2db42f1139cb45e704
NunezAN <alex101nunez@gmail.com>
2022-09-21 10:42:00 -0500

    completed meeting times

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,20 +1,38 @@
-const productExceptSelf = (nums) => {
-  let productArray = [];
-  productArray.push(1);
-  let leftSum = 1;
-  let rightSum = 1;
-  for (let i = 0; i < nums.length-1; i++) {
-    leftSum = leftSum * nums[i];
-    // console.log(leftSum);
-    productArray.push(leftSum);
+const meetingTimes = (times) => {
+  // let canMeet = true;
+  // let meetingTimes = new Set();
+  // for (i = 0; i < times.length; i++) {
+  //   let meetingStart=times[i][0];
+  //   let meetingEnd = times[i][1];
+  //   while(meetingStart<=meetingEnd)
+  //   {
+  //     if(meetingTimes.has(meetingStart)){
+  //       return false;
+  //     }
+  //     meetingTimes.add(meetingStart);
+  //     meetingStart++;
+  //   }
+  // }
+  // console.log(meetingTimes);
+
+  // return canMeet;
+  times.sort(function (a, b) {
+    return a - b;
+  });
+  for(i=1;i<times.length;i++){
+    meetingStart=times[i][0];
+    meetingEnd = times[i-1][1];
+    if(meetingStart < meetingEnd){
+      return false;
+    }
   }
-  for (let i = nums.length-2; i >=0; i--) {
-    rightSum = rightSum * nums[i+1];
-    console.log(rightSum);
-    productArray[i]=rightSum*productArray[i];
-  }
-  // console.log(leftProducts,rightProducts);
-  return productArray;
+  console.log(times);
+  return true;
 };
 
-console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(
+  meetingTimes([
+    [10, 15],
+    [20, 25],
+  ])
+);
--- [DSA] Files/questions/meetingTimes/index.js
+++ [DSA] Files/questions/meetingTimes/index.js
@@ -1,17 +1,44 @@
 /** Meeting Times **
- * 
+ *
  * Given an array of meeting times, determine if a person can
- * attend all meetings. 
- *  
+ * attend all meetings.
+ *
  * @example
  * meetingTimes([[10, 15], [20, 25]]) -> true
  * meetingTimes([[5, 10], [10, 15], [12, 25], [25, 30]]) -> false
  * meetingTimes([[10, 20], [20, 30], [30, 40]]) -> true
- * 
+ *
  */
 
- const meetingTimes = (times) => {
+const meetingTimes = (times) => {
+//   let canMeet = true;
+//   let meetingTimes = new Set();
+//   for (i = 0; i < times.length; i++) {
+//     let meetingStart = times[i][0];
+//     let meetingEnd = times[i][1];
+//     while (meetingStart <= meetingEnd) {
+//       if (meetingTimes.has(meetingStart)) {
+//         return false;
+//       }
+//       meetingTimes.add(meetingStart);
+//       meetingStart++;
+//     }
+//   }
+//   console.log(meetingTimes);
 
- };
+//   return canMeet;
+times.sort(function (a, b) {
+    return a - b;
+  });
+  for(i=1;i<times.length;i++){
+    meetingStart=times[i][0];
+    meetingEnd = times[i-1][1];
+    if(meetingStart < meetingEnd){
+      return false;
+    }
+  }
+  console.log(times);
+  return true;
+};
 
 module.exports = meetingTimes;
