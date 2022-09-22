d5fc81f862c85363e69067bd21de4f2f7073ca75
NunezAN <alex101nunez@gmail.com>
2022-09-22 10:38:30 -0500

    completed searchRotatedArray

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,18 +1,32 @@
-const findMinimum = (nums) => {
-  let l = 0;
-  let r = nums.length - 1;
-  let mid = 0;
-  while (l < r) {
-    console.log("top", l, r);
-    mid = Math.floor((l + r) / 2);
-    if (nums[mid] > nums[r]) {
-      l = mid + 1;
-    } else {
-      r = mid;
+const search = (nums, target) => {
+  l = 0;
+  r = nums.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if(nums[mid]===target)
+    {
+      return mid;
     }
-    console.log("bottom", l, r);
-  }
-  return nums[l];
-};
 
-console.log(findMinimum([21, 40 , 99, 102, 1102, 18, 20]));
+    if (nums[mid] < nums[r]) {
+      if (nums[r] < target || target < nums[mid]) {
+        r = mid - 1;
+      }
+      else {
+        l = mid + 1;
+      }
+    } 
+    else
+    {
+      if(nums[l]>target|| target >nums[mid])
+      {
+        l=mid+1;
+      }
+      else{
+        r=mid-1;
+      }
+    }
+  }
+  return "not found";
+};
+console.log(search([5, 4, 1, 2, 3], 2));
--- [DSA] Files/questions/searchRotatedArray/index.js
+++ [DSA] Files/questions/searchRotatedArray/index.js
@@ -1,20 +1,48 @@
 /** Search in Rotated Sorted Array **
- * 
- * Given a rotated sorted array and a target, 
- * return the index of the number in the array 
+ *
+ * Given a rotated sorted array and a target,
+ * return the index of the number in the array
  * which equals to the target.
- * 
- * Note: Your solutions' time complexity must be faster than O(n). 
- * 
+ *
+ * Note: Your solutions' time complexity must be faster than O(n).
+ *
  * @example
  * search([5, 4, 1, 2, 3], 2) -> 3
  * search([7, 9, 12, 3, 4], 7) -> 0
  * search([3, 4, 2, 0], 4) -> 1
- * 
+ *
  */
 
 const search = (nums, target) => {
+    l = 0;
+  r = nums.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if(nums[mid]===target)
+    {
+      return mid;
+    }
 
-}
+    if (nums[mid] < nums[r]) {
+      if (nums[r] < target || target < nums[mid]) {
+        r = mid - 1;
+      }
+      else {
+        l = mid + 1;
+      }
+    } 
+    else
+    {
+      if(nums[l]>target|| target >nums[mid])
+      {
+        l=mid+1;
+      }
+      else{
+        r=mid-1;
+      }
+    }
+  }
+  return "not found";
+};
 
 module.exports = search;
