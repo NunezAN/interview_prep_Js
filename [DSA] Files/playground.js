62bed85de0c350b126ce5354c6f1fb1dfc1895e8
NunezAN <alex101nunez@gmail.com>
2022-09-23 17:09:30 -0500

    completed moveZeroes

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,22 +1,16 @@
-const flatten = (nums) => {
-  const result = []
-
-  const flattenArray = (nums) => {
-    for(elem of nums){
-      if(Array.isArray(elem)){
-        console.log("is array",elem);
-        flattenArray(elem);
-      }
-      else{
-        result.push(elem);
-      }
+const moveZeroes = (nums) => {
+  let l =0;
+  let r=0;
+  while(r<nums.length){
+    if(nums[r]!=0)
+    {
+      let temp =nums[r];
+      nums[r]=nums[l];
+      nums[l] = temp
+      l++;
     }
+    r++;
   }
-
-  flattenArray(nums)
-
-  return result
-
-
- };
-console.log(flatten([1, [2, 3, [4, [5]]]]));
+  return nums;
+};
+console.log(moveZeroes([1, 0, 2, 0]));
--- [DSA] Files/questions/moveZeroes/index.js
+++ [DSA] Files/questions/moveZeroes/index.js
@@ -12,7 +12,19 @@
  */
 
  const moveZeroes = (nums) => {
-
+    let l=0;
+    let r=0;
+    while (r<nums.length) 
+    {
+        if(nums[r]!==0){
+            let temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+        }
+        r++;
+    }
+    return nums;
 }
 
 module.exports = moveZeroes;
