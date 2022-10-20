359609927532edfde0b2509854da22deb1c21656
NunezAN <alex101nunez@gmail.com>
2022-10-20 09:17:44 -0500

    completed max-char

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -49,10 +49,9 @@ const maxCharacter = (str) => {
   const charMap = new Map();
   let maxChar = "";
   let maxCount = 0;
-  for (const char of str) {
-    count = charMap.get(char);
+  for (char of str) {
+    const count = charMap.get(char);
     charMap.set(char, count + 1 || 1);
-    console.log(charMap.get(char));
     if (maxCount < charMap.get(char)) {
       maxCount = charMap.get(char);
       maxChar = char;
