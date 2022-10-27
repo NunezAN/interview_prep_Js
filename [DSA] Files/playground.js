c281975c7be60f93cf1704e7786c84620df27a43
NunezAN <alex101nunez@gmail.com>
2022-10-26 21:17:38 -0500

    completed threeSum

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,18 +1,32 @@
-const stocks = (prices) => {
-  let l = 0;
-  let r = 1;
-  let maxProfit = 0;
-  while (r < prices.length) {
-    const profit = prices[r] - prices[l];
-    if (profit > 0) {
-      if (maxProfit < profit) {
-        maxProfit = profit;
+function threeSum(nums) {
+  nums = nums.sort(function (a, b) {
+    return a - b;
+  });
+  const threeSumArray = [];
+  i = 0;
+  l = 1;
+  r = nums.length - 1;
+  while (i < nums.length - 2) {
+    if (nums[i] !== nums[i-1]) {
+      while (l < r) {
+        const sum = nums[i] + nums[l] + nums[r];
+        if (sum === 0) {
+          threeSumArray.push([nums[i], nums[l], nums[r]]);
+          l++;
+          while (nums[l] === nums[l - 1] && l < r) {
+            l++;
+          }
+        } else if (sum < 0) {
+          l++;
+        } else if (sum > 0) {
+          r--;
+        }
       }
-    } else {
-      l = r;
     }
-    r++;
+    i++;
+    l = i + 1;
+    r = nums.length - 1;
   }
-  return maxProfit;
-};
-console.log(stocks([8, 3, 6, 1, 6, 4, 7]));
+  return threeSumArray;
+}
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
--- [DSA] Files/questions/threeSum/index.js
+++ [DSA] Files/questions/threeSum/index.js
@@ -12,22 +12,51 @@
  */
 
 function threeSum(nums) {
-  solutionArray = [];
+  // solutionArray = [];
+  // nums = nums.sort(function (a, b) {
+  //   return a - b;
+  // });
+  // // console.log(nums);
+  // i = 0;
+  // l = i + 1;
+  // r = nums.length - 1;
+  // while (i < nums.length - 2) {
+  //   // (console.log(negative);
+  //   if (nums[i] !== nums[i - 1]) {
+  //     while (l < r) {
+  //       // console.log(i,l,r);
+  //       let sum = nums[l] + nums[r] + nums[i];
+  //       if (0 === sum) {
+  //         solutionArray.push([nums[i], nums[l], nums[r]]);
+  //         l++;
+  //         while (nums[l] === nums[l - 1] && l < r) {
+  //           l++;
+  //         }
+  //       } else if (sum < 0) {
+  //         l++;
+  //       } else if (sum > 0) {
+  //         r--;
+  //       }
+  //     }
+  //   }
+  //   i++;
+  //   l = i + 1;
+  //   r = nums.length - 1;
+  // }
+  // return solutionArray;
   nums = nums.sort(function (a, b) {
     return a - b;
   });
-  // console.log(nums);
+  const threeSumArray = [];
   i = 0;
-  l = i + 1;
+  l = 1;
   r = nums.length - 1;
   while (i < nums.length - 2) {
-    // (console.log(negative);
     if (nums[i] !== nums[i - 1]) {
       while (l < r) {
-        // console.log(i,l,r);
-        let sum = nums[l] + nums[r] + nums[i];
-        if (0 === sum) {
-          solutionArray.push([nums[i], nums[l], nums[r]]);
+        const sum = nums[i] + nums[l] + nums[r];
+        if (sum === 0) {
+          threeSumArray.push([nums[i], nums[l], nums[r]]);
           l++;
           while (nums[l] === nums[l - 1] && l < r) {
             l++;
@@ -43,7 +72,7 @@ function threeSum(nums) {
     l = i + 1;
     r = nums.length - 1;
   }
-  return solutionArray;
+  return threeSumArray;
 }
 
 //DO NOT EDIT BELOW THIS LINE
