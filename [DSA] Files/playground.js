343dc824729c299b7c94df337148c25da0e94f96
NunezAN <alex101nunez@gmail.com>
2022-10-31 12:59:57 -0500

    completed containerWithMostWater

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,19 +1,20 @@
-const search = (nums, target) => {
-  l = 0;
-  r = nums.length - 1;
-  let mid = Math.floor((l + r) / 2);
-  while (l <= r) {
-    if (nums[mid] === target) {
-      return mid;
+const maxWater = (heights) => {
+  let l = 0;
+  let r = heights.length - 1;
+  let maxArea = 0;
+  while (l < r) {
+    const width = r - l;
+    const height = Math.min(heights[l], heights[r]);
+    const area = height * width;
+    if (area > maxArea) {
+      maxArea = area;
     }
-    if(nums[mid]>target||target>nums[r]){
-      r=mid-1;
+    if (height[l] < height[r]) {
+      l++;
+    } else {
+      r--;
     }
-    else{
-      l=mid+1;
-    }
-    mid = Math.floor((l + r) / 2);
   }
-  return 0;
+  return maxArea;
 };
-console.log(search([5, 4, 1, 2, 3], 2));
+console.log(maxWater([1, 5, 6, 3, 4, 2]) );
--- [DSA] Files/questions/containerWithMostWater/index.js
+++ [DSA] Files/questions/containerWithMostWater/index.js
@@ -1,40 +1,57 @@
 /** Container with Most Water **
- * 
+ *
  * Given an array 'heights' which represents the heights of blocks
  * on a container, find the maximum area of water that can be stored
- * within these blocks. 
- * 
+ * within these blocks.
+ *
  * @example
  * maxWater([1, 5, 4, 3]) -> 6
  * maxWater([1, 5, 6, 3, 4, 2]) -> 12
  * maxWater([4, 3, 2, 1, 4]) -> 16
- * 
+ *
  */
 
- const maxWater = (heights) => {
-   let l=0;
-   let r=heights.length-1;
-   let maxArea=0;
+const maxWater = (heights) => {
+  // let l=0;
+  // let r=heights.length-1;
+  // let maxArea=0;
 
-   while(l<r){
-      const height = Math.min(heights[l],heights[r])
-      const width = r-l;
-      const area = height * width;
+  // while(l<r){
+  //    const height = Math.min(heights[l],heights[r])
+  //    const width = r-l;
+  //    const area = height * width;
 
-      if(area >maxArea){
-         maxArea = area;
-      }
+  //    if(area >maxArea){
+  //       maxArea = area;
+  //    }
 
-      if(heights[l]<heights[r])
-      {
-         l++;
-      }
-      else{
-         r--
-      }
-   }
+  //    if(heights[l]<heights[r])
+  //    {
+  //       l++;
+  //    }
+  //    else{
+  //       r--
+  //    }
+  // }
 
-   return maxArea;
-}
+  // return maxArea;
+  let l = 0;
+  let r = heights.length - 1;
+  let maxArea = 0;
+  while (l < r) {
+    const width = r - l;
+    const height = Math.min(heights[l], heights[r]);
+    const area = height * width;
+    if (area > maxArea) {
+      maxArea = area;
+    }
+    if (heights[l] < heights[r]) {
+      l++;
+    } else {
+      r--;
+    }
+  }
+  return maxArea;
+};
 
 module.exports = maxWater;
