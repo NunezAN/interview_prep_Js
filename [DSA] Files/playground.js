eda833129728e0c84438ae10a35c1fc9e95420e5
NunezAN <alex101nunez@gmail.com>
2022-10-31 10:56:00 -0500

    completed searchRotatedArray

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,15 +1,19 @@
-const findMinimum = (nums) => {
+const search = (nums, target) => {
   l = 0;
   r = nums.length - 1;
   let mid = Math.floor((l + r) / 2);
-  while (l < r) {
-    if (nums[mid] > nums[r]) {
-      l = mid + 1;
-    } else {
-      r = mid;
+  while (l <= r) {
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if(nums[mid]>target||target>nums[r]){
+      r=mid-1;
+    }
+    else{
+      l=mid+1;
     }
     mid = Math.floor((l + r) / 2);
   }
-  return nums[l];
+  return 0;
 };
-console.log(findMinimum([5, 4, 1, 2]));
+console.log(search([5, 4, 1, 2, 3], 2));
--- [DSA] Files/questions/searchRotatedArray/index.js
+++ [DSA] Files/questions/searchRotatedArray/index.js
@@ -14,35 +14,51 @@
  */
 
 const search = (nums, target) => {
-    l = 0;
+  //   l = 0;
+  // r = nums.length - 1;
+  // while (l <= r) {
+  //   const mid = Math.floor((l + r) / 2);
+  //   if(nums[mid]===target)
+  //   {
+  //     return mid;
+  //   }
+
+  //   if (nums[mid] < nums[r]) {
+  //     if (nums[r] < target || target < nums[mid]) {
+  //       r = mid - 1;
+  //     }
+  //     else {
+  //       l = mid + 1;
+  //     }
+  //   } 
+  //   else
+  //   {
+  //     if(nums[l]>target|| target >nums[mid])
+  //     {
+  //       l=mid+1;
+  //     }
+  //     else{
+  //       r=mid-1;
+  //     }
+  //   }
+  // }
+  // return "not found";
+  l = 0;
   r = nums.length - 1;
+  let mid = Math.floor((l + r) / 2);
   while (l <= r) {
-    const mid = Math.floor((l + r) / 2);
-    if(nums[mid]===target)
-    {
+    if (nums[mid] === target) {
       return mid;
     }
-
-    if (nums[mid] < nums[r]) {
-      if (nums[r] < target || target < nums[mid]) {
-        r = mid - 1;
-      }
-      else {
-        l = mid + 1;
-      }
-    } 
-    else
-    {
-      if(nums[l]>target|| target >nums[mid])
-      {
-        l=mid+1;
-      }
-      else{
-        r=mid-1;
-      }
+    if(nums[mid]>target||target>nums[r]){
+      r=mid-1;
     }
+    else{
+      l=mid+1;
+    }
+    mid = Math.floor((l + r) / 2);
   }
-  return "not found";
+  return 0;
 };
 
 module.exports = search;
