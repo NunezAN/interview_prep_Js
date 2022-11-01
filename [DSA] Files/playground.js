714cfb8e657181309467746b73f049510192788b
NunezAN <alex101nunez@gmail.com>
2022-11-01 11:55:13 -0500

    completed flatten array

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,20 +1,14 @@
-const maxWater = (heights) => {
-  let l = 0;
-  let r = heights.length - 1;
-  let maxArea = 0;
-  while (l < r) {
-    const width = r - l;
-    const height = Math.min(heights[l], heights[r]);
-    const area = height * width;
-    if (area > maxArea) {
-      maxArea = area;
-    }
-    if (height[l] < height[r]) {
-      l++;
-    } else {
-      r--;
+const flatten = (nums) => {
+  const flattenArr = [];
+  const flattenFunc = (nums) =>{
+    for(const num of nums){
+      if(Array.isArray(num)){
+        flattenFunc(num);
+      }
+      else(flattenArr.push(num));
     }
   }
-  return maxArea;
+  flattenFunc(nums);
+  return flattenArr;
 };
-console.log(maxWater([1, 5, 6, 3, 4, 2]) );
+console.log(flatten([1, 2, 3, [4, 5]]));
--- [DSA] Files/questions/flattenArray/index.js
+++ [DSA] Files/questions/flattenArray/index.js
@@ -1,35 +1,43 @@
 /** Flatten Array **
- * 
+ *
  * Given an array 'nums' that can have nested arrays inside it,
- * flatten the array. 
- *  
- * 
+ * flatten the array.
+ *
+ *
  * @example
  * flatten([1, 2, 3, [4, 5]]) -> [1, 2, 3, 4, 5]
  * flatten([1, 2, 3, [4, [5]]]) -> [1, 2, 3, 4, 5]
  * flatten([1, [2, 3, [4, [5]]]]) -> [1, 2, 3, 4, 5]
- * 
+ *
  */
 
- const flatten = (nums) => {
-  const result = []
+const flatten = (nums) => {
+  // const result = [];
 
-  const flattenArray = (nums) => {
-    for(elem of nums){
-      if(Array.isArray(elem)){
-        flattenArray(elem);
+  // const flattenArray = (nums) => {
+  //   for (elem of nums) {
+  //     if (Array.isArray(elem)) {
+  //       flattenArray(elem);
+  //     } else {
+  //       result.push(elem);
+  //     }
+  //   }
+  // };
+
+  // flattenArray(nums);
+
+  // return result;
+  const flattenArr = [];
+  const flattenFunc = (nums) =>{
+    for(const num of nums){
+      if(Array.isArray(num)){
+        flattenFunc(num);
       }
-      else{
-        result.push(elem);
-      }
+      else(flattenArr.push(num));
     }
   }
+  flattenFunc(nums);
+  return flattenArr;
+};
 
-  flattenArray(nums)
-
-  return result
-
-
- };
-
-module.exports = flatten; 
+module.exports = flatten;
