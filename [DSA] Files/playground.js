81a06626407a22e4a8532a8c0cee4a4a3b4df495
NunezAN <alex101nunez@gmail.com>
2022-11-01 19:57:41 -0500

    testing git on new sys

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,14 +1,14 @@
 const flatten = (nums) => {
-  const flattenArr = [];
+  const flatten = [];
   const flattenFunc = (nums) =>{
     for(const num of nums){
       if(Array.isArray(num)){
         flattenFunc(num);
       }
-      else(flattenArr.push(num));
+      else(flatten.push(num));
     }
   }
   flattenFunc(nums);
-  return flattenArr;
+  return flatten;
 };
 console.log(flatten([1, 2, 3, [4, 5]]));
