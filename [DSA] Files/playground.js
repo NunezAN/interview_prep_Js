67176e40069506295c5956eeb8c80cfda5eb5a43
NunezAN <alex101nunez@gmail.com>
2022-11-02 18:49:30 -0500

    completed sum of two

--- [DSA] Files/playground.js
+++ [DSA] Files/playground.js
@@ -1,30 +1,13 @@
-const moveZeroes = (nums) => {
-  let l = 0;
-  let r = nums.length - 1;
-  while (l < r) {
-    if (nums[l] === 0) {
-      while(nums[r]===0){
-        r--;
-      }
-      const temp = nums[r];
-      nums[r] = nums[l];
-      nums[l] = temp;
-      r--;
-    }
-    l++;
+const sumOfTwo = (nums1, nums2, target) => {
+  const remainderSet = new Set();
+  for(num of nums1){
+    remainderSet.add(target-num)
   }
-  return nums;
-  // let l = 0;
-  // let r = 0;
-  // while (r < nums.length) {
-  //   if (nums[r] !== 0) {
-  //     let temp = nums[l];
-  //     nums[l] = nums[r];
-  //     nums[r] = temp;
-  //     l++;
-  //   }
-  //   r++;
-  // }
-  // return nums;
+  for(num of nums2){
+    if(remainderSet.has(num)){
+      return true;
+    }
+  }
+  return false;
 };
-console.log(moveZeroes([0, 0, 1, 2, 3]));
+console.log( sumOfTwo([1, 2, 3], [5, 2], 8));
--- [DSA] Files/questions/sumOfTwo/index.js
+++ [DSA] Files/questions/sumOfTwo/index.js
@@ -1,28 +1,38 @@
 /** Sum of Two **
- * 
- * Given two arrays, 'nums1' and 'nums2', return true if 
+ *
+ * Given two arrays, 'nums1' and 'nums2', return true if
  * there are two numbers in both arrays which can add up to
- * an integer target. Otherwise, return false. 
- * 
+ * an integer target. Otherwise, return false.
+ *
  * @example
  * sumOfTwo([1, 2, 3], [5, 2], 8) -> true
  * sumOfTwo([0, 12, 8], [3, 1, 4], 2) -> false
  * sumOfTwo([4, 5, 8], [3, 1, 4], 9) -> true
- * 
+ *
  */
 
- const sumOfTwo = (nums1, nums2, target) => {
-  let remainderSet = new Set();
-  for(elem of nums1){
-    remainderSet.add(target-elem);
-  }
+const sumOfTwo = (nums1, nums2, target) => {
+  // let remainderSet = new Set();
+  // for(elem of nums1){
+  //   remainderSet.add(target-elem);
+  // }
 
-  for(elem of nums2){
-    if(remainderSet.has(elem)){
+  // for(elem of nums2){
+  //   if(remainderSet.has(elem)){
+  //     return true;
+  //   }
+  // }
+  // return false;
+  const remainderSet = new Set();
+  for (num of nums1) {
+    remainderSet.add(target - num);
+  }
+  for (num of nums2) {
+    if (remainderSet.has(num)) {
       return true;
     }
   }
   return false;
- };
+};
 
 module.exports = sumOfTwo;
