179d6fe1bb3a8a8ef658e1f8aa9f3aac6ff44d09
NunezAN <alex101nunez@gmail.com>
2023-01-16 08:26:21 -0600

    completed insertionSort

--- [DSA] Files/questions/insertionSort/index.js
+++ [DSA] Files/questions/insertionSort/index.js
@@ -1,16 +1,25 @@
 /** Insertion Sort **
- * 
+ *
  * Sort the 'nums' array using insertion sort.
- *  
+ *
  * @example
  * insertionSort([3, 2, 1]) -> [1, 2, 3]
  * insertionSort([0, 12, 8]) -> [0, 8, 12]
  * insertionSort([3, 4, 0, 2]) -> [0, 2, 3, 4]
- * 
+ *
  */
 
- const insertionSort = (nums) => {
-    
-}
+const insertionSort = (nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    let current = nums[i];
+    let j = i - 1;
+    while (j >= 0 && current < nums[j]) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = current;
+  }
+    return nums
+};
 
 module.exports = insertionSort;
