e76e2bec42e69814448a97ad72a13e175e6039d3
NunezAN <alex101nunez@gmail.com>
2023-01-25 08:34:22 -0600

    completed threeSum

--- [DSA] Files/questions/threeSum/index.js
+++ [DSA] Files/questions/threeSum/index.js
@@ -44,26 +44,26 @@ function threeSum(nums) {
   //   r = nums.length - 1;
   // }
   // return solutionArray;
-  nums = nums.sort((a, b) => {
-    return a - b;
-  });
-  const threeSumArray = [];
-  i = 0;
-  l = 1;
-  r = nums.length - 1;
+  nums = nums.sort((a, b) => a - b);
+  let i = 0;
+  let l = 1;
+  let r = nums.length - 1;
+  let result = [];
   while (i < nums.length - 2) {
     if (nums[i] !== nums[i - 1]) {
       while (l < r) {
         const sum = nums[i] + nums[l] + nums[r];
         if (sum === 0) {
-          threeSumArray.push([nums[i], nums[l], nums[r]]);
+          result.push([nums[i], nums[l], nums[r]]);
           l++;
           while (nums[l] === nums[l - 1] && l < r) {
             l++;
           }
-        } else if (sum < 0) {
+        }
+        if (sum < 0) {
           l++;
-        } else if (sum > 0) {
+        }
+        if (sum > 0) {
           r--;
         }
       }
@@ -72,7 +72,7 @@ function threeSum(nums) {
     l = i + 1;
     r = nums.length - 1;
   }
-  return threeSumArray;
+  return result;
 }
 
 //DO NOT EDIT BELOW THIS LINE
--- playground.js
+++ playground.js
@@ -1,13 +1,32 @@
-const sumOfTwo = (nums1, nums2, target) => {
-  sumSet = new Set();
-  for (num of nums1) {
-    sumSet.add(target - num);
-  }
-  for (num of nums2) {
-    if (sumSet.has(num)) {
-      return true;
+function threeSum(nums) {
+  nums = nums.sort((a, b) => a - b);
+  let i = 0;
+  let l = 1;
+  let r = nums.length - 1;
+  let result = [];
+  while (i < nums.length - 2) {
+    if (nums[i] !== nums[i - 1]) {
+      while (l < r) {
+        const sum = nums[i] + nums[l] + nums[r];
+        if (sum === 0) {
+          result.push([nums[i], nums[l], nums[r]]);
+          l++;
+          while (nums[l] === nums[l - 1] && l < r) {
+            l++;
+          }
+        }
+        if (sum < 0) {
+          l++;
+        }
+        if (sum > 0) {
+          r--;
+        }
+      }
     }
+    i++;
+    l = i + 1;
+    r = nums.length - 1;
   }
-  return false;
-};
-console.log(sumOfTwo([1, 2, 3], [5, 2], 8));
+  return result;
+}
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
